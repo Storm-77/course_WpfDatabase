4f0d357f42add327c7981f8b3d772e03601dcadf
MilkyStorm3 <czechu600@gmail.com>
2022-03-18 21:34:19 +0100

    reference viewmodels

---
+++ DbProject/commands/MessageCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DbProject.commands
+{
+    class MessageCommand : CommandBase
+    {
+        public readonly string m_message;
+
+        public MessageCommand(string message)
+        {
+            m_message = message;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public override void Execute(object parameter)
+        {
+            MessageBox.Show(m_message, "yo",MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ DbProject/viewModels/MenuViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DbProject.viewModels
+{
+    class MenuViewModel : ViewModelBase
+    {
+
+        public MenuViewModel()
+        {
+            SomeCommand = new commands.MessageCommand("that is my message to you");
+        }
+
+        private ICommand m_someCommand;
+        public ICommand SomeCommand
+        {
+            get
+            {
+                return m_someCommand;
+            }
+            set
+            {
+                m_someCommand = value;
+                this.OnPropertyChanged(nameof(SomeCommand));
+            }
+        }
+    }
+}
